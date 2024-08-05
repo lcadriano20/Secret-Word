248470edc2ab7a6b78016ce26b382b605ca316af
AdrianoC <adriano.cbagc@gmail.com>
2024-08-05 13:39:37 -0300

    Commit inicial

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Volante", "Farol", "Para-brisa", "Bateria", "Freio"],
+  fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Abacaxi", "Manga", "Morango", "Melancia", "Uva"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Orelha", "Boca", "Nariz", "Mão", "Pé"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Placa-mãe", "Processador", "Memória RAM", "HD", "SSD", "Placa de vídeo"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React", "Python", "API", "Node.js", "HTML", "CSS", "Angular"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Pão", "Queijo", "Frango", "Batata", "Tomate"],
+};
